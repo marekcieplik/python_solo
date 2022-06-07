4c0b31a285fcf895714798f8718069730bde29b6
marekcieplik <marek.cieplik@gmail.com>
2022-06-07 09:23:00 +0200

    Add example of pure function. Add code formating PEP 8

--- Functional_Programming.py
+++ Functional_Programming.py
@@ -7,3 +7,16 @@ def multiple(x):
 
 
 print(multiple_twice(multiple, 2))  # ((x * x) + (x * x)) ** 2
+
+# pure function - no side effects, and return value thad depends only on their arguments
+
+
+def pure_function(x, y):
+    return x * y + x
+
+
+some_list = []
+
+
+def impure(arg):  # this function changed the state of some_list
+    some_list.append(arg)
--- Functional_Programming_test.py
+++ Functional_Programming_test.py
@@ -1,11 +1,13 @@
 import unittest
-import Functional_Programming as fp
+import Functional_Programming as f_P
+
 
 class MyTestCase(unittest.TestCase):
     def test_multiple(self):
-        self.assertEqual(fp.multiple(2), 4)  # add assertion here
+        self.assertEqual(f_P.multiple(2), 4)  # add assertion here
+
     def test_multiple_twice(self):
-        self.assertEqual((fp.multiple_twice(fp.multiple, 2)), 64)
+        self.assertEqual((f_P.multiple_twice(fp.multiple, 2)), 64)
 
 
 if __name__ == '__main__':
